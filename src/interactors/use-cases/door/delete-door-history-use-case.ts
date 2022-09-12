8dd1bedb4d7dc4d76f8b50b6abb5c50c990be9d6
ahmed4040400 <ahmed4040400@gmail.com>
2022-09-12 18:02:15 +0200

    added new error to throw when the door owner not authorized

--- src/interactors/use-cases/door/delete-door-history-use-case.ts
+++ src/interactors/use-cases/door/delete-door-history-use-case.ts
@@ -4,7 +4,6 @@ import { IDoorHistoryAuthorizer } from 'src/contracts/interactors/authorizers/do
 import { IDeleteHistoryUseCase } from 'src/contracts/interactors/use-cases/door/delete-door-history-use-case.interface';
 import { DoorEventOutData } from 'src/entities/dtos/door-event/door-event-output';
 
-//TODO: implement an authorizer to make sure that the event is deleted by an authorized user
 export class DeleteDoorHistoryUseCase implements IDeleteHistoryUseCase {
   constructor(
     private deleteFromHistoryRepo: IDeleteFromHistoryRepository,
@@ -14,7 +13,6 @@ export class DeleteDoorHistoryUseCase implements IDeleteHistoryUseCase {
     doorOwnerId: string,
     eventId: string,
   ): Promise<DoorEventOutData> {
-    //TODO: implement an authorizer to compare the userId to the event creator
     const authorized = await this.doorHistoryAuthorizer.authorize(
       doorOwnerId,
       eventId,
